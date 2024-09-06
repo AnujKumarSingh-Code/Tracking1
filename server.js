0989a630d7d15c3b2efa04baab7708cb7052f76b
Anuj Kumar <133045218+AnujKumarSingh-Code@users.noreply.github.com>
2024-09-06 12:32:32 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // MongoDB connection (replace with your MongoDB URI)
-mongoose.connect('mongodb+srv://anujkumarsinghcoder:QgSvKNYjniJWzg0F@project-next.amlt0ce.mongodb.net/?retryWrites=true&w=majority&appName=project-next');
+mongoose.connect('mongodb+srv://anujkumarsinghcoder:QgSvKNYjniJWzg0F@project-anal.amlt0ce.mongodb.net/?retryWrites=true&w=majority&appName=project-anal');
 
 // MongoDB Schema for tracking clicks
 const clickSchema = new mongoose.Schema({
